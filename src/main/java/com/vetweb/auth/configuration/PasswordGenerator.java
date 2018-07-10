9a941df661ee2ca3bbbfe85983f1fac2b7289341
renan.rodrigues <renan.rodrigues@metasix.com.br>
2018-07-10 16:59:26 -0300

    Autenticação e autorização

---
+++ src/main/java/com/vetweb/auth/configuration/PasswordGenerator.java
@@ -0,0 +1,22 @@
+package com.vetweb.auth.configuration;
+
+import java.security.MessageDigest;
+
+import org.jboss.security.Base64Encoder;
+
+public class PasswordGenerator {
+	
+	public static void main(String[] args) {
+		System.out.println(new PasswordGenerator().genPassword("Callable"));
+	}
+	
+	public String genPassword(String password) {
+		try {
+			byte[] digest = MessageDigest.getInstance("sha-256").digest(password.getBytes());
+			return Base64Encoder.encode(digest);
+		} catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+}
