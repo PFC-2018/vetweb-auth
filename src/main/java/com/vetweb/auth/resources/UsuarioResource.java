f917fc2e52e216ed93b83710a918fbb1cf9cc59b
Renan Rodrigues <renan.rodrigues@metasix.com.br>
2018-07-17 21:00:27 -0300

    Correção da importação do @PathParam

--- src/main/java/com/vetweb/auth/resources/UsuarioResource.java
+++ src/main/java/com/vetweb/auth/resources/UsuarioResource.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.websocket.server.PathParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -18,7 +18,6 @@ import com.vetweb.auth.model.Usuario;
 @RequestScoped
 @Path("usuarios")
 @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-//http://localhost:8080/vetweb-auth/service/usuarios/all
 public class UsuarioResource {
 	
 	@Inject
