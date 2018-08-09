8f31729b158c9d63be42e714f60bbf3ae3fb3eb9
Renan Rodrigues <renan.rodrigues@metasix.com.br>
2018-08-08 21:37:38 -0300

    Corrigindo codificação da senha

--- src/main/java/com/vetweb/auth/configuration/PasswordGenerator.java
+++ src/main/java/com/vetweb/auth/configuration/PasswordGenerator.java
@@ -1,20 +1,23 @@
 package com.vetweb.auth.configuration;
 
-import java.security.MessageDigest;
-
-import javax.xml.bind.DatatypeConverter;
+import org.jboss.security.auth.spi.Util;
 
 public class PasswordGenerator {
 	
 	public static void main(String[] args) {
 		System.out.println(PasswordGenerator.genPassword("vetweb"));
+		String pass = Util.createPasswordHash("MD5", Util.BASE64_ENCODING, null, null, "vetweb");
+		String passRecepcao = Util.createPasswordHash("MD5", Util.BASE64_ENCODING, null, null, "recepcao");
+		System.out.println(pass);
+		System.out.println(passRecepcao);
 	}
 	
 	public static String genPassword(String password) {
 		try {
-			MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-			messageDigest.update(password.getBytes());
-			return DatatypeConverter.printHexBinary(messageDigest.digest()).toLowerCase();
+			return Util.createPasswordHash("MD5", Util.BASE64_ENCODING, null, null, password);
+//			MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+//			messageDigest.update(password.getBytes());
+//			return DatatypeConverter.printHexBinary(messageDigest.digest()).toLowerCase();
 		} catch (Exception exception) {
 			throw new RuntimeException(exception);
 		}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,7 @@
-﻿use authdata;
-
-insert into perfil (descricao) values ('recepcao');
+﻿insert into perfil (descricao) values ('recepcao');
 insert into perfil (descricao) values ('cliente');	
 insert into perfil (descricao) values ('veterinario');
+insert into perfil (descricao) values ('ADMIN');
 
 insert into permissao (id, url) values (1, '/clientes/**');
 insert into permissao (id, url) values (2, '/usuarios/**');
@@ -17,10 +16,16 @@ insert into perfil_permissao (perfis_descricao, permissoes_id) values ('veterina
 insert into perfil_permissao (perfis_descricao, permissoes_id) values ('veterinario', 2);
 insert into perfil_permissao (perfis_descricao, permissoes_id) values ('veterinario', 3);
 insert into perfil_permissao (perfis_descricao, permissoes_id) values ('veterinario', 4);
-insert into perfil_permissao (perfis_descricao, permissoes_id) values ('veterinario', 5);	
+insert into perfil_permissao (perfis_descricao, permissoes_id) values ('veterinario', 5);
+insert into perfil_permissao (perfis_descricao, permissoes_id) values ('ADMIN', 1);
+insert into perfil_permissao (perfis_descricao, permissoes_id) values ('ADMIN', 2);
+insert into perfil_permissao (perfis_descricao, permissoes_id) values ('ADMIN', 3);
+insert into perfil_permissao (perfis_descricao, permissoes_id) values ('ADMIN', 4);
+insert into perfil_permissao (perfis_descricao, permissoes_id) values ('ADMIN', 5);
 
-insert into usuario (username, email, password) values ('renan.rodrigues', 'renan.rodrigues@metasix.com.br', 'MTIzNDU2');
-insert into usuario (username, email, password) values ('recepcao', 'renanfr1047@gmail.com', 'cmVjZXBjYW8=');
+insert into usuario (username, email, password) values ('vetweb', 'renan.rodrigues@metasix.com.br', '8idxMVxgdswZlsuLyr9Skw==');
+insert into usuario (username, email, password) values ('recepcao', 'renanfr1047@gmail.com', 'zEjsMlsw93bJOg6XHN0k/Q==');
 
-insert into usuario_perfil (usuarios_username, perfis_descricao) values ('renan.rodrigues', 'veterinario');
+insert into usuario_perfil (usuarios_username, perfis_descricao) values ('vetweb', 'veterinario');
+insert into usuario_perfil (usuarios_username, perfis_descricao) values ('vetweb', 'ADMIN');
 insert into usuario_perfil (usuarios_username, perfis_descricao) values ('recepcao', 'recepcao');
